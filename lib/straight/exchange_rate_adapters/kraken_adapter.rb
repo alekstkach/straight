967d1e13eef2bdf1a504b2e3586f5ad79a32f606
aleks tkach <ale.tkachenko@gmail.com>
2015-01-26 20:44:23 -0600

    created KrakenAdapter added exception NilValueNotAllowed for rate in ExchangeRate::Adapter, created #rate_to_f method in ExchangeRate::Adapter, altered ExchangeRate::KrakenAdapter#rate_for with #rate_to_f, added specs

---
+++ lib/straight/exchange_rate_adapters/kraken_adapter.rb
@@ -0,0 +1,20 @@
+module Straight
+  module ExchangeRate
+
+    class KrakenAdapter < Adapter
+      
+      FETCH_URL = 'https://api.kraken.com/0/public/Ticker?pair=xbtusd'
+
+      def rate_for(currency_code)
+        super
+        if rate = @rates['result']['XXBTZ' + currency_code.upcase]
+          rate_to_f(rate['p'].last)
+        else
+          raise CurrencyNotSupported
+        end
+      end
+
+    end
+
+  end
+end
